a2c467c70f9439c1fbaa10d67adab7d85e58650a
LAPTOP-012PRSTU\yuran <angyuran@gmail.com>
2021-07-19 16:58:26 +0800

    URL (cart.html), HTML injection(reviews.js) hover to reveal flag (home.html), download files (products.html)

--- HoneyHax_Bakery/FirstExpressServer/Model/reviews.js
+++ HoneyHax_Bakery/FirstExpressServer/Model/reviews.js
@@ -8,68 +8,103 @@ var db = require('./databaseConfig');
 
 var reviewsDB = {
 
-    //GET /product/:productId/reviews/
-    getReviews: function (productId, callback) {
-        var dbConn = db.getConnection();
-        dbConn.connect(function (err) {
-            if (err) {
-                console.log(err);
-                return callback(err, null);
+    // GET /reviews
+    processReview: function (name, review, callback) {
+        var args = [name, review]
+        var regExp = /<a\s*.*>\s*.*<\/a>/g;
+        var filtered = [];
+        var attack = false;
+        args.forEach(function (item) {
+            if (item.includes("<script>") && item.includes('</script>')) {
+                attack = true;
+                item = item.replace('<script>', '');
+                item = item.replace('</script>', '');
+                filtered.push(item);
+            }
+
+            else {filtered.push(item)}
+        }),
+
+        args.forEach(function(item){
+            // if(regExp==args){
+            if(regExp.test(args)){
+                attack = "true"
+                // filtered.push(item);
             } else {
-                var sql = "select r.reviewContent, r.rating, u.username from reviews r, users u where r.userId=u.userId";
-                dbConn.query(sql, [productId], function (err, result) {
-                    dbConn.end();
-                    if (err) {
-                        console.log(err);
-                    } else {
-                        console.log(result);
-                    }
-                    return callback(err, result);
-                });
+
+                filtered.push(item);
             }
-        });
-    },
 
 
-    //GET /product/:productId/reviews/:reviewId
-    getReviewsById: function (productId, reviewId, callback) {
-        var dbConn = db.getConnection();
-        dbConn.connect(function (err) {
-            if (err) {
-                console.log(err);
-                return callback(err, null);
-            } else {
-                var sql = "select r.reviewContent, r.rating, u.username from reviews r, users u where r.userId=u.userId and r.reviewId=? ";
-                dbConn.query(sql, [productId, reviewId], function (err, result) {
-                    dbConn.end();
-                    if (err) {
-                        console.log(err);
-                    } else {
-                        console.log(result);
-                    }
-                    return callback(err, result);
-                });
-            }
         });
+
+        console.log(filtered, attack)
+        return callback(null, [filtered[0], filtered[1], attack])
+
+
+
+
+
     },
 
-    // POST /users/:userId/:productId/review (Create reviews)
-    insertReviews: function (reviewContent, rating, productId, userId, callback) {   
-        var dbConn = db.getConnection();
-        dbConn.connect(function (err) {
-            if (err) {
-                console.log(err);
-                return callback(err, null);
-            }
-            else {
-                var sql = "insert into reviews (reviewContent, rating, productId, userId) Values(?,?,?,?)";
-                dbConn.query(sql, [reviewContent, rating, productId, userId], function (err, result) {
-                    dbConn.end();
-                    return callback(err, result);
-                });
-            }
-        });
-    },
+    // injectReviews: function (name,review,callback){
+    //     var input = [name,review]
+    //     var filtered = [];
+    //     var regExp = ("<a\s*.*>\s*.*<\/a>/g");
+    //     var res = "true";
+    //     input.forEach(function(item){
+    //         if(regExp.test(input)){
+    //             res = "false"
+    //             console.log("a tag is not allowed!");
+    //         }else{
+
+    //             console.log(item);
+    //         }
+    //         console.log(filtered,res);
+            
+    //     });
+
+    // }
+
+    // //GET /product/:productId/reviews/:reviewId
+    // getReviewsById: function (productId, reviewId, callback) {
+    //     var dbConn = db.getConnection();
+    //     dbConn.connect(function (err) {
+    //         if (err) {
+    //             console.log(err);
+    //             return callback(err, null);
+    //         } else {
+    //             var sql = "select r.reviewContent, r.rating, u.username from reviews r, users u where r.userId=u.userId and r.reviewId=? ";
+    //             dbConn.query(sql, [productId, reviewId], function (err, result) {
+    //                 dbConn.end();
+    //                 if (err) {
+    //                     console.log(err);
+    //                 } else {
+    //                     console.log(result);
+    //                 }
+    //                 return callback(err, result);
+    //             });
+    //         }
+    //     });
+    // },
+
+    // // POST /users/:userId/:productId/review (Create reviews)
+    // insertReviews: function (reviewContent, rating, productId, userId, callback) {   
+    //     var dbConn = db.getConnection();
+    //     dbConn.connect(function (err) {
+    //         if (err) {
+    //             console.log(err);
+    //             return callback(err, null);
+    //         }
+    //         else {
+    //             var sql = "insert into reviews (reviewContent, rating, productId, userId) Values(?,?,?,?)";
+    //             dbConn.query(sql, [reviewContent, rating, productId, userId], function (err, result) {
+    //                 dbConn.end();
+    //                 return callback(err, result);
+    //             });
+    //         }
+    //     });
+    // },
 
 
 }
