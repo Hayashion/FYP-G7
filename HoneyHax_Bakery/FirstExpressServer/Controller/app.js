07da02a34bba44d7c1236bb7e0a4a3de3a1e52cd
SNORLAXX2009 <82882875+SNORLAXX2009@users.noreply.github.com>
2021-07-24 15:58:30 +0800

    updated about page (location to staff)

--- HoneyHax_Bakery/FirstExpressServer/Controller/app.js
+++ HoneyHax_Bakery/FirstExpressServer/Controller/app.js
@@ -343,13 +343,13 @@ app.get('/products/:productName/', function (req, res) {
 
 });
 
-//Search /location/:locationName/
-app.get('/location/:locationName/', function (req, res) {
+//Search /staff/:staffName/
+app.get('/staff/:staffName/', function (req, res) {
 
-    var locationName = req.params.locationName;
-    console.log(locationName)
+    var staffName = req.params.staffName;
+    console.log(staffName)
 
-    productsDB.searchLocation(locationName, function (err, result) {
+    productsDB.searchStaff(staffName, function (err, result) {
         
 
         res.type('json');
